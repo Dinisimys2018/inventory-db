a98b629bd9ebba331e5ac5ba4d92a0a5d2a89f6f
dinisimys2018 <dinisimys2018@gmail.com>
2026-04-06 11:49:03 +0300

    add pools

--- src/imm_mem_table.zig
+++ src/imm_mem_table.zig
@@ -10,9 +10,10 @@ pub fn ImmMemTableType(comptime EntryType: type) type {
 
         entries: std.MultiArrayList(EntryType),
 
-        pub fn init(allocator: std.mem.Allocator, max_count: usize) !ImmMemTable {
+        /// entries_max_count - common between ImmMemTable and MemTable
+        pub fn init(allocator: std.mem.Allocator, entries_max_count: usize) !ImmMemTable {
             return .{
-                .entries = try .initCapacity(allocator, max_count),
+                .entries = try .initCapacity(allocator, entries_max_count),
             };
         }
 
@@ -29,20 +30,20 @@ pub fn ImmMemTableType(comptime EntryType: type) type {
 pub fn ImmMemTablePoolType(comptime EntryType: type) type {
     return struct {
         const ImmMemTablePool = @This();
-
-        tables: ArrayList(ImmMemTableType(EntryType)),
+        const ImmMemTable = ImmMemTableType(EntryType);
+        tables: ArrayList(ImmMemTable),
         free: []u6,
         filled: []u6,
 
-        pub fn init(allocator: std.mem.Allocator, max_count_tables: u8, max_count_entries: usize) !ImmMemTablePool {
-            var tables: ArrayList(ImmMemTableType(EntryType)) =  try .initCapacity(allocator, max_count_tables);
+        pub fn init(allocator: std.mem.Allocator, max_count_mem_tables: u8, max_count_entries: usize) !ImmMemTablePool {
+            var tables: ArrayList(ImmMemTable) =  try .initCapacity(allocator, max_count_mem_tables);
 
-            const free = try allocator.alloc(u6, max_count_tables);
-            const filled = try allocator.alloc(u6, max_count_tables);
+            const free = try allocator.alloc(u6, max_count_mem_tables);
+            const filled = try allocator.alloc(u6, max_count_mem_tables);
 
-            for (0..max_count_tables) |index| {
-                const table: ImmMemTableType(EntryType) = try .init(allocator, max_count_entries);
-                tables.appendAssumeCapacity(table, max_count_tables);
+            for (0..max_count_mem_tables) |index| {
+                const table: ImmMemTable = try .init(allocator, max_count_entries);
+                tables.appendAssumeCapacity(table, max_count_mem_tables);
                 free[index] = index;
             }
 
@@ -53,13 +54,29 @@ pub fn ImmMemTablePoolType(comptime EntryType: type) type {
             };
         }
 
-        pub fn fillOneTable(imm_mem_table_pool: ImmMemTablePool, mem_table: *MemTableType(EntryType)) void {
-            const free_table_index = imm_mem_table_pool.free.len - 1;
-            imm_mem_table_pool.free.len -= 1;
-            assert(imm_mem_table_pool.free.lenimm_mem_table_pool.free.len > 0);
+        pub fn deinit(imm_mem_tables_pool: *ImmMemTablePool, allocator: std.mem.Allocator) void {
+            imm_mem_tables_pool.tables.deinit(allocator);
+            imm_mem_tables_pool.* = undefined;
+        }
+
+        pub fn fillOneFreeTable(imm_mem_tables_pool: *ImmMemTablePool, mem_table: *MemTableType(EntryType)) void {
+            const free_table_index = imm_mem_tables_pool.getFreeTableIndex();
             
-            imm_mem_table_pool.tables.items[free_table_index].fill(mem_table);
-            imm_mem_table_pool.filled[imm_mem_table_pool.filled + 1] = free_table_index;
+            assert(imm_mem_tables_pool.tables.items[free_table_index].entries.capacity == mem_table.entries.capacity);
+
+            // flush entries from mem_table to imm_mem_table via index
+            imm_mem_tables_pool.tables.items[free_table_index].fill(mem_table);
+            // move imm_mem_table from free list to filled list
+            imm_mem_tables_pool.filled[imm_mem_tables_pool.filled + 1] = free_table_index;
+        }
+
+        fn getFreeTableIndex(imm_mem_tables_pool: *ImmMemTablePool) usize {
+            assert(imm_mem_tables_pool.free.len > 0);
+            //Get element from end free list
+            const free_table_index = imm_mem_tables_pool.free.len - 1;
+            imm_mem_tables_pool.free.len -= 1;
+
+            return free_table_index;
         }
     };
 }
--- src/mem_table.zig
+++ src/mem_table.zig
@@ -1,32 +1,127 @@
 const std = @import("std");
+const ArrayList = std.ArrayList;
+const assert = std.testing.assert;
+
 const ImmMemTablePoolType = @import("imm_mem_table.zig").ImmMemTablePoolType;
+const testing = std.testing;
 
 pub fn MemTableType(comptime EntryType: type) type {
     return struct {
         const MemTable = @This();
 
-        entries: std.MultiArrayList(EntryType),
-        max_count: usize,
+        entries: std.MultiArrayList(EntryType) = .empty,
 
-        pub fn init(allocator: std.mem.Allocator, max_count: usize) !MemTable {
+        pub fn init(allocator: std.mem.Allocator, entries_max_count: usize) !MemTable {
             return .{
-                .entries = try .initCapacity(allocator, max_count),
-                .size = 0,
-                .max_count = max_count,
+                .entries = try .initCapacity(allocator, entries_max_count),
             };
         }
 
-        pub fn addEntries(mem_table: *MemTable, entries: []*EntryType, imm_mem_table_pool: ImmMemTablePoolType(EntryType)) void {
+        pub fn deinit(mem_table: *MemTable, allocator: std.mem.Allocator) void {
+            mem_table.entries.deinit(allocator);
+            mem_table.* = undefined;
+        }
+
+        pub fn fill(mem_table: *MemTable, entries: []*EntryType) void {
+            assert(mem_table.entries.len <= mem_table.entries.capacity);
+
             for (entries) |entry| {
-                if (mem_table.entries.len == mem_table.max_count) {
-                    mem_table.flush(imm_mem_table_pool);
-                }
                 mem_table.entries.appendAssumeCapacity(entry);
             }
         }
+    };
+}
 
-        fn flush(mem_table: *MemTable, imm_mem_table_pool: ImmMemTablePoolType(EntryType)) void {
-            imm_mem_table_pool.fillOneTable(mem_table);
+pub fn MemTablePoolType(comptime EntryType: type) type {
+    return struct {
+        const MemTablePool = @This();
+        const MemTable = MemTableType(EntryType);
+
+        tables: ArrayList(MemTable),
+        active_table_index: u6,
+        free: []u6,
+        filled: []u6,
+
+        pub fn init(allocator: std.mem.Allocator, mem_tables_max_count: u8, entries_max_count: usize) !MemTablePool {
+            var tables: ArrayList(MemTable) = try .initCapacity(allocator, mem_tables_max_count);
+
+            const free = try allocator.alloc(u6, mem_tables_max_count);
+            const filled = try allocator.alloc(u6, mem_tables_max_count);
+
+            for (0..mem_tables_max_count) |index| {
+                const table: MemTable = try .init(allocator, entries_max_count);
+                tables.appendAssumeCapacity(table, mem_tables_max_count);
+                free[index] = index;
+            }
+
+            return .{
+                .tables = tables,
+                .active_table_index = 0,
+                .free = free,
+                .filled = filled,
+            };
+        }
+
+        pub fn deinit(mem_tables_pool: *MemTablePool, allocator: std.mem.Allocator) void {
+            mem_tables_pool.tables.deinit(allocator);
+            mem_tables_pool.* = undefined;
+        }
+
+        pub fn addEntries(mem_tables_pool: *MemTablePool, entries: []*EntryType) void {
+            var entries_count = 0;
+            var entries_offset: usize = 0;
+            var input_entries = entries;
+
+            while (entries_count <= entries.len) {
+                const active_table = mem_tables_pool.tables.items[mem_tables_pool.active_table_index];
+
+                entries_offset = active_table.entries.capacity - active_table.entries.len;
+                entries_count += entries_offset;
+                active_table.fill(input_entries);
+
+                if (entries_offset <= input_entries.len) {
+                    mem_tables_pool.filled[mem_tables_pool.filled.len + 1] = mem_tables_pool.active_table_index;
+                    mem_tables_pool.active_table_index = mem_tables_pool.getFreeTableIndex();
+                }
+
+                input_entries = input_entries[entries_offset..];
+            }
+        }
+
+        fn getFreeTableIndex(mem_tables_pool: *MemTablePool) usize {
+            assert(mem_tables_pool.free.len > 0);
+            //Get element from end free list
+            const free_table_index = mem_tables_pool.free.len - 1;
+            mem_tables_pool.free.len -= 1;
+
+            return free_table_index;
         }
     };
+}
+
+const OrderItemRow = @import("entities.zig").OrderItemRow;
+
+test "mem_table" {
+    const allocator = std.testing.allocator;
+    const Entity = OrderItemRow;
+    const entries_max_count = 2;
+    const mem_tables_max_count = 2;
+
+    const mem_table_pool: MemTablePoolType(Entity) = try .init(
+        allocator,
+        mem_tables_max_count,
+        entries_max_count,
+    );
+    defer mem_table_pool.deinit(allocator);
+
+    const input_entries: std.ArrayList(Entity) = try .initCapacity(allocator, entries_max_count);
+
+    for (0..entries_max_count) |i| {
+        input_entries.appendAssumeCapacity(.{
+            .id = i,
+            .order_id = i * 2,
+        });
+    }
+
+    mem_table_pool.addEntries(input_entries);
 }
