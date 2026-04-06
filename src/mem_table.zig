c2ff820df7c258a4d750d682d86ef4727aa95f95
dinisimys2018 <dinisimys2018@gmail.com>
2026-04-06 16:16:50 +0300

    refactoring

--- src/mem_table.zig
+++ src/mem_table.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 const ArrayList = std.ArrayList;
-const assert = std.testing.assert;
+const assert = std.debug.assert;
 const testing = std.testing;
 
 const ImmMemTablePoolType = @import("imm_mem_table.zig").ImmMemTablePoolType;
@@ -9,7 +9,7 @@ pub fn MemTableType(comptime EntryType: type) type {
     return struct {
         const MemTable = @This();
 
-        entries: std.MultiArrayList(EntryType) = .empty,
+        entries: std.MultiArrayList(EntryType),
 
         pub fn init(allocator: std.mem.Allocator, entries_max_count: usize) !MemTable {
             return .{
@@ -23,8 +23,6 @@ pub fn MemTableType(comptime EntryType: type) type {
         }
 
         pub fn fill(mem_table: *MemTable, entries: []EntryType) void {
-            assert(mem_table.entries.len <= mem_table.entries.capacity);
-
             for (entries) |entry| {
                 mem_table.entries.appendAssumeCapacity(entry);
             }
@@ -32,70 +30,95 @@ pub fn MemTableType(comptime EntryType: type) type {
     };
 }
 
-pub fn MemTablePoolType(comptime EntryType: type) type {
+pub fn MemTablePoolType(comptime EntryType: type, mem_tables_max_count: comptime_int) type {
+    const Index = u8;
+
+    comptime {
+        assert(mem_tables_max_count <= std.math.maxInt(Index));
+    }
+
     return struct {
         const MemTablePool = @This();
         const MemTable = MemTableType(EntryType);
 
-        tables: ArrayList(MemTable),
-        active_table_index: u6,
-        free: []u6,
-        filled: []u6,
+        tables: ArrayList(*MemTable),
+        free: [mem_tables_max_count]bool,
+        filled: [mem_tables_max_count]bool,
+        active_table_index: Index = 0,
 
-        pub fn init(allocator: std.mem.Allocator, mem_tables_max_count: u8, entries_max_count: usize) !MemTablePool {
-            var tables: ArrayList(MemTable) = try .initCapacity(allocator, mem_tables_max_count);
+        pub fn init(allocator: std.mem.Allocator, entries_max_count: usize) !MemTablePool {
+            var tables: ArrayList(*MemTable) = try .initCapacity(allocator, mem_tables_max_count);
+            errdefer tables.deinit(allocator);
 
-            const free = try allocator.alloc(u6, mem_tables_max_count);
-            const filled = try allocator.alloc(u6, mem_tables_max_count);
-            var index: u6 = 0;
+            var index: Index = 0;
+            const free: [mem_tables_max_count]bool = .{true} ** mem_tables_max_count;
+            const filled: [mem_tables_max_count]bool = .{false} ** mem_tables_max_count;
 
-            while (index < mem_tables_max_count): (index += 1) {
-                const table: MemTable = try .init(allocator, entries_max_count);
+            while (index < mem_tables_max_count) : (index += 1) {
+                var table = try allocator.create(MemTable);
+                errdefer allocator.destroy(table);
+                table.* = try .init(allocator, entries_max_count);
+                errdefer table.deinit(allocator);
+
                 tables.appendAssumeCapacity(table);
-                free[index] = index;
             }
 
             return .{
                 .tables = tables,
-                .active_table_index = 0,
                 .free = free,
                 .filled = filled,
+                .active_table_index = 0,
             };
         }
 
         pub fn deinit(mem_tables_pool: *MemTablePool, allocator: std.mem.Allocator) void {
+            for (mem_tables_pool.tables.items) |table| {
+                table.deinit(allocator);
+                allocator.destroy(table);
+            }
             mem_tables_pool.tables.deinit(allocator);
             mem_tables_pool.* = undefined;
         }
 
         pub fn addEntries(mem_tables_pool: *MemTablePool, entries: []EntryType) void {
-            var entries_count: usize = 0;
-            var entries_offset: usize = 0;
-            var input_entries = entries;
+            var entries_start: usize = 0;
+            var entries_end: usize = 0;
 
-            while (entries_count <= entries.len) {
+            while (entries_end < entries.len) {
+                // Сразу убираем активную таблицу из свободных,
+                // чтобы другие вызовы не получили доступ к ней
+                mem_tables_pool.free[mem_tables_pool.active_table_index] = false;
                 var active_table = mem_tables_pool.tables.items[mem_tables_pool.active_table_index];
+                // Получаем количество, которое мы можем вставить в активную таблицу
+                const rest = active_table.entries.capacity - active_table.entries.len;
+                entries_end += rest;
 
-                entries_offset = active_table.entries.capacity - active_table.entries.len;
-                entries_count += entries_offset;
-                active_table.fill(input_entries);
-
-                if (entries_offset <= input_entries.len) {
-                    mem_tables_pool.filled[mem_tables_pool.filled.len + 1] = mem_tables_pool.active_table_index;
-                    mem_tables_pool.active_table_index = mem_tables_pool.getFreeTableIndex();
+                if (entries_end >= entries.len) {
+                    entries_end = entries.len;
                 }
 
-                input_entries = input_entries[entries_offset..];
+                std.debug.print("\nentries_start = {d}| entries_end = {d}| rest = {d}\n", .{entries_start, entries_end , rest});
+                std.debug.print("\ninput entries = \n{any}\n======\n", .{entries[entries_start..entries_end]});
+
+                active_table.fill(entries[entries_start..entries_end]);
+
+                // Если мы заполнили все свободное место
+                // значит перемещаем активную таблицу в filled 
+                if (rest == entries_end - entries_start) {
+                    mem_tables_pool.filled[mem_tables_pool.active_table_index] = true;
+
+                    // Если есть еще свободные таблицы,
+                    // тогда смещаем индекс для работы с новой активной таблицой
+                    if (mem_tables_pool.active_table_index < mem_tables_pool.filled.len - 1) {
+                        mem_tables_pool.active_table_index += 1;
+                    } else {
+                        //TODO full-filled all mem_tables
+                        unreachable;
+                    }
+                }
+
+                entries_start = entries_end;
             }
-        }
-
-        fn getFreeTableIndex(mem_tables_pool: *MemTablePool) usize {
-            assert(mem_tables_pool.free.len > 0);
-            //Get element from end free list
-            const free_table_index = mem_tables_pool.free.len - 1;
-            mem_tables_pool.free.len -= 1;
-
-            return free_table_index;
         }
     };
 }
@@ -107,18 +130,19 @@ test "mem_table" {
     const Entity = OrderItemRow;
     const entries_max_count = 2;
     const mem_tables_max_count = 2;
-
-    var mem_table_pool: MemTablePoolType(Entity) = try .init(
+    const MemTablePool = MemTablePoolType(Entity, mem_tables_max_count);
+    var mem_table_pool:MemTablePool = try .init(
         allocator,
-        mem_tables_max_count,
         entries_max_count,
     );
     defer mem_table_pool.deinit(allocator);
+    const entries_count = entries_max_count * mem_tables_max_count - 1;
+    var input_entries: std.ArrayList(Entity) = try .initCapacity(allocator, entries_count);
+    defer input_entries.deinit(allocator);
 
-    var input_entries: std.ArrayList(Entity) = try .initCapacity(allocator, entries_max_count);
     var index: u8 = 0;
 
-    while(index < entries_max_count): (index += 1) {
+    while(index < entries_count): (index += 1) {
         input_entries.appendAssumeCapacity(.{
             .id = index,
             .order_id = index * 2,
@@ -126,4 +150,6 @@ test "mem_table" {
     }
 
     mem_table_pool.addEntries(input_entries.items);
+    std.debug.print("free: {any}\n", .{mem_table_pool.free}); 
+    std.debug.print("filled: {any}\n", .{mem_table_pool.filled}); 
 }
